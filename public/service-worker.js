4791447e39fdbd6f84e4f461d54db0adac08f297
paul <paul.m.johnson1988@gmail.com>
2025-07-01 15:04:42 +1200

    service

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,8 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // Only cache GET requests
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     caches.open('v1').then(cache => {
       return cache.match(event.request).then(response => {
