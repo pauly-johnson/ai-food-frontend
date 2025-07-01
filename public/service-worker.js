69e6e1c73b9e574f2bece2c11591cf4c933ab84b
paul <paul.m.johnson1988@gmail.com>
2025-07-01 14:49:49 +1200

    update build

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+// Simple custom service worker for offline support
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('v1').then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
